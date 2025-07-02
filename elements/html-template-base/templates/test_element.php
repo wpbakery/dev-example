4503c7ba51ee8e4e13f6cefc96b963e833f32d65
OlegApanovich <dollar4444@gmail.com>
2025-07-02 11:59:01 +0300

    WPB-1679 code style

--- elements/html-template-base/templates/test_element.php
+++ elements/html-template-base/templates/test_element.php
@@ -7,7 +7,7 @@
  * @var WPBakeryShortCode_Test_Element $this
  */
 
-$attributes = ( shortcode_atts( array(
+$attributes = ( shortcode_atts( [
 	'value'       => 100,
 	'bgcolor'     => '#e0e4e1',
 	'actcolor'    => '#9fcb61',
@@ -16,13 +16,13 @@ $attributes = ( shortcode_atts( array(
 	'strokecolor' => '#fff',
 	'strokewidth' => 2,
 	'innerfill'   => 50,
-	'radius'      => 50
-), $atts ) );
+	'radius'      => 50,
+], $atts ) );
 
 $value = (int) $attributes['value'];
 if ( $value > 100 ) {
 	$value = 100;
-} else if ( $value < 0 ) {
+} elseif ( $value < 0 ) {
 	$value = 0;
 }
 
@@ -30,7 +30,8 @@ if ( $value > 100 ) {
 wp_enqueue_script( 'test_element' );
 
 // Enqueue Custom CSS Style
-//wp_enqueue_style( 'test_element_css' ); // Must be previously registered in constructor (class WPBakeryShortCode_Test_Element extends WPBakeryShortCode).
+// Must be previously registered in constructor (class WPBakeryShortCode_Test_Element extends WPBakeryShortCode).
+wp_enqueue_style( 'test_element_css' );
 
 // attributes for html
 $radius       = 2 * (int) $attributes['radius'];
@@ -38,8 +39,8 @@ $val1         = $value;
 $val2         = 100 - $value;
 $col1         = $attributes['actcolor'];
 $col2         = $attributes['bgcolor'];
-$animation    = ( $this->check_bool( $attributes['cssanim'] ) ? "true" : "false" );
-$show_stroke  = ( $this->check_bool( $attributes['showstroke'] ) ? "true" : "false" );
+$animation    = ( $this->check_bool( $attributes['cssanim'] ) ? 'true' : 'false' );
+$show_stroke  = ( $this->check_bool( $attributes['showstroke'] ) ? 'true' : 'false' );
 $stroke_color = $attributes['strokecolor'];
 $stroke_width = (int) $attributes['strokewidth'];
 $inner_fill   = (int) $attributes['innerfill'];
@@ -47,18 +48,18 @@ $inner_fill   = (int) $attributes['innerfill'];
 ?>
 
 <canvas class="piechartex" height="<?php echo $radius; ?>"
-        width="<?php echo $radius; ?>"
-        data-value-first="<?php echo $val1; ?>"
-        data-value-second="<?php echo $val2; ?>"
-        data-value-first-color="<?php echo $col1; ?>"
-        data-value-second-color="<?php echo $col2; ?>"
-        data-animation="<?php echo $animation; ?>"
-        data-segment-stroke-show="<?php echo $show_stroke; ?>"
-        data-segment-stroke-color="<?php echo $stroke_color; ?>"
-        data-segment-stroke-width="<?php echo $stroke_width; ?>"
-        data-percentage-inner-cutout="<?php echo $inner_fill; ?>"></canvas>
+		width="<?php echo $radius; ?>"
+		data-value-first="<?php echo $val1; ?>"
+		data-value-second="<?php echo $val2; ?>"
+		data-value-first-color="<?php echo $col1; ?>"
+		data-value-second-color="<?php echo $col2; ?>"
+		data-animation="<?php echo $animation; ?>"
+		data-segment-stroke-show="<?php echo $show_stroke; ?>"
+		data-segment-stroke-color="<?php echo $stroke_color; ?>"
+		data-segment-stroke-width="<?php echo $stroke_width; ?>"
+		data-percentage-inner-cutout="<?php echo $inner_fill; ?>"></canvas>
 
 <div class="your_content_from_textarea_html">
-	<?php echo wpb_js_remove_wpautop($content, true); ?>
+	<?php echo wpb_js_remove_wpautop( $content, true ); ?>
 </div>
 
